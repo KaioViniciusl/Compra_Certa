903d8d14a4625b765105fa71e2699281364dccb8
vitor amorim <vitoralmada10@gmail.com>
2024-08-28 09:36:11 -0300

    foi consertado os detalhes do show_decription e foi consertado método delete

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :user_groups
+  has_many :user_groups, dependent: :destroy
 
   has_one_attached :photo
 
   validates :name_group, presence: true
-  validates :observation, presence: true
+  validates :description_group, presence: true
 
   def users
     user_groups.map(&:user).compact
