400f87fa3893846af9aa885f29be58ac1c4cade9
Kaio Vinicius <vinciuskaio49@gmail.com>
2024-08-30 03:45:57 -0300

    implementacao de cred e deb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,43 +12,35 @@ class User < ApplicationRecord
   has_one_attached :photo
   after_create_commit :accept_invitations
 
+  # Método para calcular o saldo total considerando todos os grupos
   def total_balance
-    groups = Group.joins(:expenses => :expense_shares).where(expense_shares: { user_id: self.id }).distinct
-
-    total_balance = 0
-
-    groups.each do |group|
-      total_balance += balance_for_group(group)
+    user_groups.sum do |user_group|
+      (user_group.credit || 0) - (user_group.debit || 0)
     end
-
-    total_balance
   end
 
+  # Método para calcular o saldo de um usuário em um grupo específico
   def balance_for_group(group)
-    expenses = group.expenses.joins(:expense_shares).where(expense_shares: { user_id: self.id })
+    user_group = user_groups.find_by(group: group)
+    return 0 unless user_group
 
-    total_due = 0
-    total_paid = 0
-
-    expenses.each do |expense|
-      total_shares = expense.expense_shares.count
-      per_user_amount = expense.amount / total_shares.to_f
-
-      total_due += per_user_amount
-      total_paid += expense.expense_shares.find_by(user_id: self.id).try(:share_amount).to_f
-    end
-
-    total_paid - total_due
+    user_group.credit - user_group.debit
   end
 
+  # Método para obter o saldo de um usuário em cada grupo
+  def group_balances
+    user_groups.includes(:group).each_with_object({}) do |user_group, balances|
+      balances[user_group.group] = user_group.credit - user_group.debit
+    end
+  end
+
+  # Método para calcular o saldo em relação a uma despesa específica
   def balance_for_expense(expense_id)
     expense = Expense.find_by(id: expense_id)
     return "Despesa não encontrada" if expense.nil?
 
     total_amount = expense.amount
-
     total_shares = expense.expense_shares.count
-
     per_user_amount = total_amount / total_shares.to_f
 
     user_share = expense.expense_shares.find_by(user_id: self.id)
