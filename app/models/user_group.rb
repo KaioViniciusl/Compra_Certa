fe854bd24154091efa1df291df14c3e313359d36
Kaio Vinicius <vinciuskaio49@gmail.com>
2024-09-03 00:37:05 -0300

    resolucao bug emails

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,6 +1,13 @@
 class UserGroup < ApplicationRecord
+  before_save :downcase_user_mail
   belongs_to :user, optional: true
   belongs_to :group
 
   validates :invite_accepted, inclusion: { in: [true, false] }
+
+  private
+
+  def downcase_user_mail
+    self.user_mail = user_mail.downcase
+  end
 end
