f60c8116d2e6ca85930ef78c637ae9571acf4f88
vitor amorim <vitoralmada10@gmail.com>
2024-09-05 22:02:04 -0300

    foi feito o edit groups

--- app/javascript/controllers/photo_upload_controller.js
+++ app/javascript/controllers/photo_upload_controller.js
@@ -4,6 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input", "preview", "editLabel"]
 
+  connect(){
+    console.log("teste")
+  }
+   
+
   previewImage(event) {
     const file = this.inputTarget.files[0]
     if (file) {
