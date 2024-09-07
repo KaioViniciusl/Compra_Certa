cee3eb36e035c7c296f7b642b119647f8fecdd94
Kaio Vinicius <vinciuskaio49@gmail.com>
2024-09-07 02:24:12 -0300

    atualizando style notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   def after_sign_in_path_for(resource)
     groups_path
   end
